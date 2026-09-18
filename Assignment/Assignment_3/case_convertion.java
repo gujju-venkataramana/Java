15d6b4cea634eb1100c2b17c12ebef824939c5ac
gujju-venkataramana <gujjuvenkataramana.25.csm@anits.edu.in>
2026-09-18 08:25:42 +0530

    Add files via upload

---
+++ Assignment/Assignment_3/case_convertion.java
@@ -0,0 +1,7 @@
+public class case_convertion {
+    public static void main(String[] args) {
+     String name=  "Case_convertion";
+     System.out.println(" uppercase:"+name.toUpperCase());
+    System.out.println(" lowercase:"+name.toLowerCase());
+    }
+}
---
+++ Assignment/Assignment_3/indexExample.java
@@ -0,0 +1,7 @@
+public class indexExample{
+    public static void main(String[] args) {
+    String name= "venky";
+    int result =name.indexOf("e");
+    System.out.println("result is "+ result);
+    }
+}
