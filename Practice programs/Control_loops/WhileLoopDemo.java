f279fb04e1ae4a9086a6dea054d5f0901528c1f9
gujju-venkataramana <gujjuvenkataramana.25.csm@anits.edu.in>
2026-09-18 07:45:25 +0530

    Add files via upload

---
+++ Practice programs/Control_loops/WhileLoopDemo.java
@@ -0,0 +1,12 @@
+public class WhileLoopDemo {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        // Print numbers from 1 to 5
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
