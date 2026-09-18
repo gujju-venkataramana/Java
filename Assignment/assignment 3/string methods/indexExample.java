aea51c15ed8bda06d13699f0a649d62fb86446a2
gujju-venkataramana <gujjuvenkataramana.25.csm@anits.edu.in>
2026-09-18 08:12:16 +0530

    Delete Assignment/assignment 3 directory

--- Assignment/assignment 3/string methods/indexExample.java
+++
@@ -1,7 +0,0 @@
-public class indexExample{
-    public static void main(String[] args) {
-    String name= "venky";
-    int result =name.indexOf("e");
-    System.out.println("result is "+ result);
-    }
-}
